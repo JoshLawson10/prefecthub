f6163f49d3733feb718e2c54841dc98acde11077
Josh Lawson <joshlawson100@outlook.com>
2026-05-15 12:27:26 +1000

    feat(members): add header to members

--- src/app/(app)/members/page.tsx
+++ src/app/(app)/members/page.tsx
@@ -1,3 +1,22 @@
+import { Separator } from "@/components/ui/separator";
+import { Header } from "@/components/ui/header";
+import { Button } from "@/components/ui/button";
+import { UserPlusIcon } from "lucide-react";
+
 export default async function MembersPage() {
-  return <div>Members — TODO Sprint 2</div>
+  return (
+    <div>
+      <Header
+        title="Members"
+        actions={
+          <>
+            <Button>
+              <UserPlusIcon /> Invite member
+            </Button>
+          </>
+        }
+      />
+      <Separator className="my-4" />
+    </div>
+  );
 }
